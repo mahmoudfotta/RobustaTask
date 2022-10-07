cc022640ee283d17c1a623bfeeecdd3dc9e31870
Mahmoud gamal <mahmoud.aboelfotoh@etisalat.com>
2022-10-07 20:01:04 +0200

    Add repositories list ui and fetch data to populate the tble vieww

--- RobustaTask/Scenes/RepositoriesList/RepositoriesListController.swift
+++ RobustaTask/Scenes/RepositoriesList/RepositoriesListController.swift
@@ -9,16 +9,45 @@ import UIKit
 
 class RepositoriesListController: UIViewController, Storyboarded {
     static var storyboardName: String = "Repositories"
+    @IBOutlet weak var tableView: UITableView!
     
 
     var presenter: RepositoriesListPresenter?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Repositories List"
-        presenter?.fetchReposList(completion: { repos, errorMessage in
-            print(repos)
-        })
+        setupUI()
+        fetchData()
     }
 
+    func setupUI() {
+        title = "Repositories List"
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func fetchData() {
+        presenter?.fetchReposList(completion: {[weak self] repos, errorMessage in
+            guard let self = self else { return }
+            if let _ = errorMessage {
+                
+            } else {
+                self.tableView.reloadData()
+            }
+        })
+    }
+}
+
+extension RepositoriesListController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter?.getReposCount() ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let repo = presenter?.getRepo(index: indexPath.row)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = repo?.fullName
+        return cell
+    }
 }
